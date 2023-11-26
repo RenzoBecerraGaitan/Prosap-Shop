abf47f8c71254f6c3879d11b4f5981a62027a2b5
unknown <sngrisa@gmail.com>
2023-11-26 19:48:34 -0300

    update fix errors with class tag name

--- src/components/Login/Form/Form.jsx
+++ src/components/Login/Form/Form.jsx
@@ -4,20 +4,20 @@ import "./Form.css";
 const Form = ({Users}) => {
     return (
         <>
-            <div className='background-login pt-5 pb-5'>
-                <div class="wrapper fadeInDown">
+            <div classNameName='background-login pt-5 pb-5'>
+                <div className="wrapper fadeInDown">
                     <div id="formContent">
-                        <div class="fadeIn first">
+                        <div className="fadeIn first">
                             <img src={Users} id="icon" alt="User Icon" />
                         </div>
                         <form>
-                            <input type="text" id="email" class="fadeIn second" name="email" placeholder="Ingrese su correo" />
-                            <input type="password" id="password" class="fadeIn third" name="password" placeholder="Ingrese la contraseña" />
-                            <input type="submit" class="fadeIn fourth" value="Ingresar" />
+                            <input type="text" id="email" className="fadeIn second" name="email" placeholder="Ingrese su correo" />
+                            <input type="password" id="password" className="fadeIn third" name="password" placeholder="Ingrese la contraseña" />
+                            <input type="submit" className="fadeIn fourth" value="Ingresar" />
                         </form>
                         <div id="formFooter">
                         <h6><b>Aun no se ha registrado?</b></h6>
-                            <a class="underlineHover" href="/register"> Cree una cuenta ahora y gratis</a>
+                            <a className="underlineHover" href="/register"> Cree una cuenta ahora y gratis</a>
                         </div>
                     </div>
                 </div>
--- src/components/Register/Form/Form.jsx
+++ src/components/Register/Form/Form.jsx
@@ -4,31 +4,31 @@ import "./Form.css";
 const Form = () => {
     return (
         <>
-            <div class="registration-form">
+            <div className="registration-form">
                 <form>
-                    <div class="form-icon">
-                        <span><i class="bi bi-person-circle"></i></span>
+                    <div className="form-icon">
+                        <span><i className="bi bi-person-circle"></i></span>
                     </div>
-                    <div class="form-group">
-                        <input type="text" class="form-control item" id="username" placeholder="Username" />
+                    <div className="form-group">
+                        <input type="text" className="form-control item" id="username" placeholder="Username" />
                     </div>
-                    <div class="form-group">
-                        <input type="password" class="form-control item" id="password" placeholder="Password" />
+                    <div className="form-group">
+                        <input type="password" className="form-control item" id="password" placeholder="Password" />
                     </div>
-                    <div class="form-group">
-                        <input type="email" class="form-control item" id="email" placeholder="Email" />
+                    <div className="form-group">
+                        <input type="email" className="form-control item" id="email" placeholder="Email" />
                     </div>
-                    <div class="form-group">
-                        <input type="text" class="form-control item" id="phone-number" placeholder="Phone Number" />
+                    <div className="form-group">
+                        <input type="text" className="form-control item" id="phone-number" placeholder="Phone Number" />
                     </div>
-                    <div class="form-group">
-                        <input type="date" class="form-control item" id="birth-date" placeholder="Birth Date" />
+                    <div className="form-group">
+                        <input type="date" className="form-control item" id="birth-date" placeholder="Birth Date" />
                     </div>
-                    <div class="form-group">
-                        <button type="button" class="btn btn-block create-account">Crear Cuenta</button>
+                    <div className="form-group">
+                        <button type="button" className="btn btn-block create-account">Crear Cuenta</button>
                     </div>
-                    <div className='text-center'>
-                    <a href='/login' className='text-white'>Volver al Login</a>
+                    <div classNameName='text-center'>
+                    <a href='/login' classNameName='text-white'>Volver al Login</a>
                     </div>
                 </form>
             </div>
