34ad15d1e280f3cd051468e808d047a3c1e537e5
RenzoBecerraGaitan <renzobecerragaitan@gmail.com>
2023-11-28 16:06:26 -0300

    loggin

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,19 @@
-import './App.css';
-import RouterConfig from './config/Router-Config';
+import "./App.css";
+import RouterConfig from "./config/Router-Config";
 import Navbar from "./components/Navbar/Navbar";
-import Footer from './components/Footer/Footer';
+import Footer from "./components/Footer/Footer";
+import { UserProvider } from "./context/UserContext";
 
 function App() {
-
   return (
     <>
-      <Navbar />
-      <RouterConfig></RouterConfig>
-      <Footer />
+      <UserProvider>
+        <Navbar />
+        <RouterConfig></RouterConfig>
+        <Footer />
+      </UserProvider>
     </>
-  )
+  );
 }
 
 export default App;
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,37 +1,79 @@
 import React from "react";
 import "./Navbar.css";
+import { useContext } from "react";
+import { UserContext } from "../../context/UserContext";
+import { singOutUser } from "../../services/firebase";
 
 export default function Navbar() {
+  const { currentUser } = useContext(UserContext);
+
   return (
     <>
       <header className="header_section bg-warning">
         <div className="container">
           <nav className="navbar navbar-expand-lg custom_nav-container">
-            <a className="navbar-brand" href="/"><img width="250" src="./../src/assets/logo.jpg" alt="#" className="logo" /></a>
-            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <a className="navbar-brand" href="/">
+              <img
+                width="250"
+                src="./../src/assets/logo.jpg"
+                alt="#"
+                className="logo"
+              />
+            </a>
+            <button
+              className="navbar-toggler"
+              type="button"
+              data-toggle="collapse"
+              data-target="#navbarSupportedContent"
+              aria-controls="navbarSupportedContent"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
               <span className=""> </span>
             </button>
-            <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <div
+              className="collapse navbar-collapse"
+              id="navbarSupportedContent"
+            >
               <ul className="navbar-nav">
                 <li className="nav-item">
-                  <a className="nav-link" href="/"><i className="bi bi-house"></i> Home </a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="/about"><i className="bi bi-file-person-fill"></i> Quienes Somos?</a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="#">
+                  <a className="nav-link" href="/">
+                    <i className="bi bi-house"></i> Home{" "}
                   </a>
                 </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="/about">
+                    <i className="bi bi-file-person-fill"></i> Quienes Somos?
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="#"></a>
+                </li>
                 <div>
-                <a className="btn btn-block btn-black" href="/login"><i className="bi bi-box-arrow-in-right"></i> Login</a>
+                  {currentUser ? (
+                    (console.log(currentUser),
+                    (
+                      <li className="currentUserContainer d-flex ">
+                        <h4> Bienvenido: {currentUser.displayName} </h4>
+                        <a
+                          className="btn btn-block btn-black"
+                          onClick={singOutUser}
+                        >
+                          <i className="bi bi-box-arrow-in-left"></i>Sing-Out
+                        </a>
+                      </li>
+                    ))
+                  ) : (
+                    <a className="btn btn-block btn-black" href="/login">
+                      <i className="bi bi-box-arrow-in-right"></i>Login
+                    </a>
+                  )}
                 </div>
-                
               </ul>
             </div>
           </nav>
         </div>
       </header>
     </>
-  )
+  );
 }
